117216943ccd13d8bb0a1449bde0816cbbd2773d
pdelaney73 <philipdelaney@gmail.com>
2025-05-28 19:44:02 -0400

    Updated HREF in Index.html

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2671552282"
+    serviceWorkerVersion: "1518912996"
   }
 });
